3d67b82ccba725c2b28bea70e7d073be85bfa28e
GoDandrea <dandrea.alves@gmail.com>
2020-11-05 18:40:38 -0300

    Adds tank controls

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody
-const MOVE_SPEED = 4
+
+const MOVE_SPEED = 3
 const MOUSE_SENS = 0.5
+
+const ROT_SENS = 2
  
 onready var anim_player = $AnimationPlayer
 onready var raycast = $RayCast
@@ -10,9 +13,9 @@ func _ready():
 	yield(get_tree(), "idle_frame")
 	get_tree().call_group("zombies", "set_player", self)
  
-func _input(event):
-	if event is InputEventMouseMotion:
-		rotation_degrees.y -= MOUSE_SENS * event.relative.x
+#func _input(event):
+#	if event is InputEventMouseMotion:
+#		rotation_degrees.y -= MOUSE_SENS * event.relative.x
  
 func _process(_delta):
 	if Input.is_action_pressed("exit"):
@@ -27,9 +30,9 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_backward"):
 		move_vec.z += 1
 	if Input.is_action_pressed("move_left"):
-		move_vec.x -= 1
+		rotation_degrees.y += ROT_SENS
 	if Input.is_action_pressed("move_right"):
-		move_vec.x += 1
+		rotation_degrees.y -= ROT_SENS
 	move_vec = move_vec.normalized()
 	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
 	move_and_collide(move_vec * MOVE_SPEED * delta)
